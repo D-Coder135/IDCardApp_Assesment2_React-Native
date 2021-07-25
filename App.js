6a27d1b240fad843ec056e57139ac714e942712b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 18:36:07 +0530

    Final Commit!

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import home from './components/home';
+import profile from './components/profile';
+import register from './components/register';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+  
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions = {{headerShown: false}}>
+          <Stack.Screen name = {'home'} component = {home} />
+           <Stack.Screen name = {'register'} component = {register} />
+           <Stack.Screen name = {'profile'} component = {profile} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // paddingTop: Constants.statusBarHeight,
     justifyContent: 'center',
   },
 });
